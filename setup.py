1d56820b91ffb3b81cfebaadf8fefbf870527fea
ogura <ogura@cello.t.u-tokyo.ac.jp>
2018-04-29 17:42:59 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='recommend_engine',
+      version='1.0',
+      py_modules=['main'],
+      install_requires=['numpy', 'matplotlib'],
+      url='https://github.com/ogura-edu/recommend-engine',
+      author='Masayoshi Ogura',
+      author_email='ogura176578@gmail.com')
